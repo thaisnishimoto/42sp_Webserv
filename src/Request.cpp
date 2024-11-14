fa48edd8ceb26c17425fda2c755e4d8d1cd14c03
Thais Mina nishimoto <thaismnishimoto@gmail.com>
2024-11-14 16:36:34 -0300

    Added parse http version

--- src/Request.cpp
+++ src/Request.cpp
@@ -18,12 +18,16 @@ void Request::initStaticMethods()
 
 void	Request::parseRequest(std::string& buffer)
 {
+	std::cout << "---------------------------------" << std::endl;
 	parseRequestLine(buffer);
+	std::cout << "status code: " << _statusCode << std::endl;
+	std::cout << "---------------------------------" << std::endl;
+	if (_statusCode != OK)
+		return ;
 }
 
 void	Request::parseRequestLine(std::string& buffer)
 {
-
 	_requestLine = getLineRN(buffer);
 	if (_requestLine.size() == 0)
 	{
@@ -34,6 +38,12 @@ void	Request::parseRequestLine(std::string& buffer)
 	std::cout << "request line: " << _requestLine << std::endl;
 	std::string requestLineCpy = _requestLine;
 	parseMethod(requestLineCpy);
+	if (_statusCode != OK)
+		return ;
+	parseTarget(requestLineCpy);
+	if (_statusCode != OK)
+		return ;
+	parseVersion(requestLineCpy);
 }
 
 
@@ -60,11 +70,33 @@ void	Request::parseMethod(std::string& requestLine)
 		_statusCode = METHOD_NOT_IMPLEMENTED;
 	}
 	else
+	{
 		_statusCode = BAD_REQUEST;
-	std::cout << "status code: " << _statusCode << std::endl;
+	}
+	std::cout << "status code after parse method: " << _statusCode << std::endl;
 
-	requestLine = requestLine.substr(_method.size(), std::string::npos);
-	std::cout << "Remainder of request line: " << requestLine << std::endl;
+	requestLine = requestLine.substr(_method.size() + 1, std::string::npos);
+	std::cout << "Remainder of request line: " << "'" << requestLine << "'" << std::endl;
 
 	//next method will need to verify if number of whitespaces are adequate
+}
+
+void Request::parseTarget(std::string &requestLine)
+{
+	_requestTarget = requestLine.substr(0, requestLine.find(" "));
+	std::cout << "request target: " << _requestTarget << std::endl;
+	if (_requestTarget.size() == 0)
+	{
+		_statusCode = BAD_REQUEST;
+	}
+	requestLine = requestLine.substr(_requestTarget.size() + 1, std::string::npos);
+	std::cout << "Remainder of request line: " << "'" << requestLine << "'" << std::endl;
+}
+
+void Request::parseVersion(std::string &requestLine)
+{
+	if (requestLine != "HTTP/1.1")
+	{
+		_statusCode = BAD_REQUEST;
+	}
 }
