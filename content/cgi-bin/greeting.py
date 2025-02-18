33e014e27c9f02741ff7c3f75580560289939cc1
thaisnishimoto <thaismnishimoto@gmail.com>
2025-02-17 23:43:08 -0300

    Refactor to remove import cgi

--- content/cgi-bin/greeting.py
+++ content/cgi-bin/greeting.py
@@ -1,12 +1,21 @@
-import cgi
+import sys
+import os
+import urllib.parse
 
 # Set content type
 print("Content-type: text/html\n")
 
-# Get form data
-form = cgi.FieldStorage()
-name = form.getvalue("name", "Guest")
-color = form.getvalue("color", "black")
+# Parse GET and POST data
+query_string = os.environ.get("QUERY_STRING", "")
+parsed_query = urllib.parse.parse_qs(query_string)
+
+content_length = int(os.environ.get("CONTENT_LENGTH", 0))
+post_data = sys.stdin.read(content_length) if content_length > 0 else ""
+parsed_post = urllib.parse.parse_qs(post_data)
+
+# Get values with default fallbacks
+name = (parsed_query.get("name") or parsed_post.get("name") or ["Guest"])[0]
+color = (parsed_query.get("color") or parsed_post.get("color") or ["black"])[0]
 
 # Print the HTML response
 print(f"""
